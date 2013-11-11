97dc0e96028438dc7778fe442e86f335eb88576c
John Was <janek.jan@gmail.com>
2013-11-11 16:55:47 +0100

    partially resolves #17 by changing the way of defining a foreign key in a migration for remote user identities

--- migrations/m130702_104658_create_table_user_remote_identity.php
+++ migrations/m130702_104658_create_table_user_remote_identity.php
@@ -6,12 +6,13 @@ class m130702_104658_create_table_user_remote_identity extends CDbMigration
 	{
 		$this->createTable('{{user_remote_identity}}', array(
 			'id'=>'pk',
-			'user_id'=>'integer NOT NULL REFERENCES {{user}} (id) ON UPDATE CASCADE ON DELETE CASCADE',
+			'user_id'=>'integer NOT NULL',
 			'provider'=>'string NOT NULL',
 			'identifier'=>'string NOT NULL',
 			'created_on'=>'timestamp NOT NULL',
 			'last_used_on'=>'timestamp',
 		));
+		$this->addForeignKey('{{user_remote_identity}}_user_id_fkey', '{{user_remote_identity}}', 'user_id', '{{user}}', 'id', 'CASCADE', 'CASCADE');
 		$this->createIndex('{{user_remote_identity}}_user_id_provider_identifier_idx', '{{user_remote_identity}}', 'user_id, provider, identifier', true);
 		$this->createIndex('{{user_remote_identity}}_user_id_idx', '{{user_remote_identity}}', 'user_id');
 	}
