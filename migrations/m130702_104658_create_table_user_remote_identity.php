a45e9d9787f19ae81375d2acbe883c1908f9ec09
John Was <janek.jan@gmail.com>
2013-11-11 16:59:46 +0100

    resolves #17, added a warning comment for mysql in migration

--- migrations/m130702_104658_create_table_user_remote_identity.php
+++ migrations/m130702_104658_create_table_user_remote_identity.php
@@ -13,6 +13,7 @@ class m130702_104658_create_table_user_remote_identity extends CDbMigration
 			'last_used_on'=>'timestamp',
 		));
 		$this->addForeignKey('{{user_remote_identity}}_user_id_fkey', '{{user_remote_identity}}', 'user_id', '{{user}}', 'id', 'CASCADE', 'CASCADE');
+		// this doesn't work in MySQL, adjust the provider and identifier columns width
 		$this->createIndex('{{user_remote_identity}}_user_id_provider_identifier_idx', '{{user_remote_identity}}', 'user_id, provider, identifier', true);
 		$this->createIndex('{{user_remote_identity}}_user_id_idx', '{{user_remote_identity}}', 'user_id');
 	}
